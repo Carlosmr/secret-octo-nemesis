18045a542297169b608d3d409e61edfb541e0e11
Javier Herrera <javhercop@gmail.com>
2012-05-23 01:55:57 +0200

    Finalizado transacciones de RepositorioProcedimientosImpl

--- src/main/java/isw2/repositorios/persistencia/RepositorioProcedimientosImpl.java
+++ src/main/java/isw2/repositorios/persistencia/RepositorioProcedimientosImpl.java
@@ -1,5 +1,6 @@
 package isw2.repositorios.persistencia;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,37 +20,162 @@ public class RepositorioProcedimientosImpl extends RepositorioJPA implements
 
 	public Procedimiento crearProcedimiento(String codigo, String nombre,
 			String descripcion) {
-		return new ProcedimientoImpl(codigo, nombre, descripcion);
+
+		Procedimiento result = new ProcedimientoImpl();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			result = new ProcedimientoImpl(codigo, nombre, descripcion);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+
+		return result;
 	}
 
 	public Set<Procedimiento> getProcedimientos() {
-		return new HashSet<Procedimiento>(getEntityManager().createQuery(
-				"from Procedimiento", Procedimiento.class).getResultList());
+
+		Set<Procedimiento> result = new HashSet<Procedimiento>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Collection<Procedimiento> clientRepository = getEntityManager()
+					.createQuery("from Procedimiento", Procedimiento.class)
+					.getResultList();
+			result = new HashSet<Procedimiento>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		} catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+
+		return result;
 	}
 
 	public Set<Procedimiento> getProcedimientosDadosDeAlta() {
 
-		return new HashSet<Procedimiento>(getEntityManager().createQuery(
-				"SELECT pr FROM Procedimiento pr WHERE pr.dadoDeBaja = false",
-				Procedimiento.class).getResultList());
+		Set<Procedimiento> result = new HashSet<Procedimiento>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Collection<Procedimiento> clientRepository = getEntityManager()
+					.createQuery(
+							"SELECT pr FROM Procedimiento pr WHERE pr.dadoDeBaja = false",
+							Procedimiento.class).getResultList();
+
+			result = new HashSet<Procedimiento>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+
+		return result;
+
 	}
 
 	public void guardar(Procedimiento procedimiento) {
-		getEntityManager().persist(procedimiento);
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			getEntityManager().persist(procedimiento);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+
 	}
 
 	public Procedimiento getProcedimiento(String codigo) {
-		return getEntityManager().find(Procedimiento.class, codigo);
+
+		Procedimiento result = new ProcedimientoImpl();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			result = getEntityManager().find(Procedimiento.class, codigo);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+
+		return result;
+
 	}
 
 	public Set<Procedimiento> getProcedimientosNoAsociadosAlTecnico(String user) {
-		Set<Procedimiento> procedimientosUsuario = getEntityManager().find(
-				Tecnico.class, user).getProcedimientos();
-		Set<Procedimiento> procedimientos = new HashSet<Procedimiento>(
-				getEntityManager().createQuery("from Procedimiento",
-						Procedimiento.class).getResultList());
-		procedimientos.removeAll(procedimientosUsuario);
-		return procedimientos;
 
+		Set<Procedimiento> result = new HashSet<Procedimiento>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Set<Procedimiento> procedimientosUsuario = getEntityManager().find(
+					Tecnico.class, user).getProcedimientos();
+			Set<Procedimiento> procedimientos = new HashSet<Procedimiento>(
+					getEntityManager().createQuery("from Procedimiento",
+							Procedimiento.class).getResultList());
+			procedimientos.removeAll(procedimientosUsuario);
+			result = procedimientos;
+
+			getEntityManager().getTransaction().commit();
+
+		} catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+
+		return result;
 	}
+
 }
