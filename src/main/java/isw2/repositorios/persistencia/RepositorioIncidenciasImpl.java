199a067b90225fa15fdeac8559fad8fd796d9083
Javier Herrera <javhercop@gmail.com>
2012-05-23 01:39:39 +0200

    repositorioIncidencia terminado sus transacciones

--- src/main/java/isw2/repositorios/persistencia/RepositorioIncidenciasImpl.java
+++ src/main/java/isw2/repositorios/persistencia/RepositorioIncidenciasImpl.java
@@ -1,5 +1,6 @@
 package isw2.repositorios.persistencia;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,64 +19,261 @@ public class RepositorioIncidenciasImpl extends RepositorioJPA implements
 	}
 
 	public Set<Incidencia> buscarIncidenciasPorDni(String dni) {
-		return new HashSet<Incidencia>(getEntityManager()
-				.createQuery("SELECT i FROM Incidencia i WHERE i.dni = :dni",
-						Incidencia.class).setParameter("dni", dni)
-				.getResultList());
+
+		Set<Incidencia> result = new HashSet<Incidencia>();
+
+		try {
+			getEntityManager().getTransaction().begin();
+			Collection<Incidencia> clientRepository = getEntityManager()
+					.createQuery(
+							"SELECT i FROM Incidencia i WHERE i.dni = :dni",
+							Incidencia.class).setParameter("dni", dni)
+					.getResultList();
+
+			result = new HashSet<Incidencia>(clientRepository);
+			getEntityManager().getTransaction().commit();
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+		}
+		return result;
+
 	}
 
 	public Set<Incidencia> buscarIncidencia(String busqueda) {
-		return new HashSet<Incidencia>(
-				getEntityManager()
-						.createQuery(
-								"SELECT i FROM Incidencia i WHERE i.nombre LIKE %:busqueda% OR i.email LIKE %:busqueda% OR i.descripcion LIKE %:busqueda%",
-								Incidencia.class)
-						.setParameter("busqueda", busqueda).getResultList());
+
+		Set<Incidencia> result = new HashSet<Incidencia>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Collection<Incidencia> clientRepository = getEntityManager()
+					.createQuery(
+							"SELECT i FROM Incidencia i WHERE i.nombre LIKE %:busqueda% OR i.email LIKE %:busqueda% OR i.descripcion LIKE %:busqueda%",
+							Incidencia.class)
+					.setParameter("busqueda", busqueda).getResultList();
+			result = new HashSet<Incidencia>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
+		return result;
 	}
 
 	public Incidencia crearIncidencia(String nombreCliente, String dni,
 			String email, String descripcion, Date fechaCompra) {
-		return new IncidenciaImpl(nombreCliente, dni, email, descripcion,
-				fechaCompra);
+
+		Incidencia result = new IncidenciaImpl();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			result = new IncidenciaImpl(nombreCliente, dni, email, descripcion,
+					fechaCompra);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
+		return result;
 	}
 
 	public Set<Incidencia> getIncidencias() {
-		return new HashSet<Incidencia>(getEntityManager().createQuery(
-				"from Incidencia", Incidencia.class).getResultList());
+
+		Set<Incidencia> result = new HashSet<Incidencia>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Collection<Incidencia> clientRepository = getEntityManager()
+					.createQuery("from Incidencia", Incidencia.class)
+					.getResultList();
+			result = new HashSet<Incidencia>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
+		return result;
 	}
 
 	public void guardar(Incidencia incidencia) {
-		getEntityManager().persist(incidencia);
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			getEntityManager().persist(incidencia);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
 	}
 
 	public Incidencia getIncidencia(Integer id) {
-		return getEntityManager().find(Incidencia.class, id);
+
+		Incidencia result = new IncidenciaImpl();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			result = getEntityManager().find(Incidencia.class, id);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
+		return result;
 	}
 
 	public Set<Incidencia> getIncidencias(String user) {
 
-		return new HashSet<Incidencia>(getEntityManager()
-				.createQuery(
-						"SELECT i FROM Incidencia i WHERE i.tecnico = :user",
-						Incidencia.class).setParameter("user", user)
-				.getResultList());
+		Set<Incidencia> result = new HashSet<Incidencia>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Collection<Incidencia> clientRepository = getEntityManager()
+					.createQuery(
+							"SELECT i FROM Incidencia i WHERE i.tecnico = :user",
+							Incidencia.class).setParameter("user", user)
+					.getResultList();
+			result = new HashSet<Incidencia>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
+		return result;
+
 	}
 
 	public Set<Incidencia> getIncidenciasSinRespuesta(String user) {
-		return new HashSet<Incidencia>(
-				getEntityManager()
-						.createQuery(
-								"SELECT i FROM Incidencia i WHERE i.respuesta = :respuesta AND i.tecnico = :user",
-								Incidencia.class)
-						.setParameter("respuesta", null)
-						.setParameter("tecnico", user).getResultList());
+
+		Set<Incidencia> result = new HashSet<Incidencia>();
+
+		try {
+
+			getEntityManager().getTransaction().begin();
+			Collection<Incidencia> clientRepository = getEntityManager()
+					.createQuery(
+							"SELECT i FROM Incidencia i WHERE i.respuesta = :respuesta AND i.tecnico = :user",
+							Incidencia.class).setParameter("respuesta", null)
+					.setParameter("tecnico", user).getResultList();
+			result = new HashSet<Incidencia>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+		return result;
+
 	}
 
 	public Set<Incidencia> getIncidenciasSinAsignar() {
-		return new HashSet<Incidencia>(getEntityManager()
-				.createQuery(
-						"SELECT i FROM Incidencia i WHERE i.tecnico = :user",
-						Incidencia.class).setParameter("user", null)
-				.getResultList());
+
+		Set<Incidencia> result = new HashSet<Incidencia>();
+
+		try {
+			getEntityManager().getTransaction().begin();
+			Collection<Incidencia> clientRepository = getEntityManager()
+					.createQuery(
+							"SELECT i FROM Incidencia i WHERE i.tecnico = :user",
+							Incidencia.class).setParameter("user", null)
+					.getResultList();
+			result = new HashSet<Incidencia>(clientRepository);
+			getEntityManager().getTransaction().commit();
+
+		}
+
+		catch (Exception oops) {
+
+			if (getEntityManager().getTransaction().isActive())
+				getEntityManager().getTransaction().rollback();
+		}
+
+		finally {
+			getEntityManager().close();
+
+		}
+
+		return result;
 	}
 }
